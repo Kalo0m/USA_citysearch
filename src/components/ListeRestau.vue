8f77c9d3afb03e0875d8d8a48a822eadefa2b3e4
e189228h <theo.letouze@etu.univ-nantes.fr>
2020-03-19 16:16:16 +0100

    theo

--- src/components/ListeRestau.vue
+++ src/components/ListeRestau.vue
@@ -10,48 +10,7 @@
             md="4"
             lg="3"
           >
-            <v-card>
-              <v-card-title class="title-container subheading font-weight-bold"><img class="floatR" :src="item.photos[0].photo.url">               {{ item.name }} </v-card-title>
-
-              <v-divider></v-divider>
-
-              <v-list dense>
-                <v-list-item>
-                  <v-list-item-content>Calories :</v-list-item-content>
-                  <v-list-item-content class="align-end">Titre</v-list-item-content>
-                </v-list-item>
-
-                <v-list-item>
-                  <v-list-item-content>Price :</v-list-item-content>
-                  <v-list-item-content class="align-end">{{ item.price_range }}/5</v-list-item-content>
-                </v-list-item>
-
-                <v-list-item>
-                  <v-list-item-content>Cuisine :</v-list-item-content>
-                  <v-list-item-content class="align-end">{{ item.cuisines }}</v-list-item-content>
-                </v-list-item>
-
-                <v-list-item>
-                  <v-list-item-content>Average price for 2 :</v-list-item-content>
-                  <v-list-item-content class="align-end">{{ item.average_cost_for_two }}</v-list-item-content>
-                </v-list-item>
-
-                <v-list-item>
-                  <v-list-item-content>Phone :</v-list-item-content>
-                  <v-list-item-content class="align-end">{{ item.phone_numbers }}</v-list-item-content>
-                </v-list-item>
-
-                <v-list-item>
-                  <v-list-item-content>is delivering now :</v-list-item-content>
-                  <v-list-item-content class="align-end">{{ item.is_delivering_now }}</v-list-item-content>
-                </v-list-item>
-
-                <v-list-item>
-                  <v-list-item-content>highlight :</v-list-item-content>
-                  <v-list-item-content class="align-end">{{ item.highlights[0] }}</v-list-item-content>
-                </v-list-item>
-              </v-list>
-            </v-card>
+            <RestauItem v-bind:restaurant="item"/>
           </v-col>
         </v-row>
     </div>
@@ -60,20 +19,24 @@
 <script>
   
   import zomato from "../zomato.js";
+  import RestauItem from './RestauItem';
 
   export default {
     
     name: 'ListeRestau',
+    components: {
+      RestauItem
+    },
     props : ['ville'],
     data: () => ({
-      restaurants : []
+      restaurants : [],
+      exemple : 'contenu'
     }),
     
     mounted() {
       const ville = this.ville.split(',')[0];
       console.log('premier element : '+ville);
       zomato.getRestaurantsByCity(ville).then(data=>{
-        console.log('aaaa');
         this.restaurants = data.restaurants.map(item=>item.restaurant);
         console.log('restaus apres modif : ');
         console.log(this.restaurants);
@@ -82,11 +45,7 @@
   }
   </script>
   <style>
-  .floatR{
-    float:right;
-    width: 75px;
-    height:75px;
-  }
+  
   .container{
     margin:2%
   }
---
+++ src/components/RestauItem.vue
@@ -0,0 +1,76 @@
+<template>
+    <v-card>
+      
+          
+             <v-card-title class="title-container subheading font-weight-bold"><img class="floatR" :src="restaurant.photos[0].photo.url">               {{ restaurant.name }} </v-card-title>
+
+              <v-divider></v-divider>
+
+              <v-list dense>
+                <v-list-item>
+                  <v-list-item-content>Calories :</v-list-item-content>
+                  <v-list-item-content class="align-end">Titre</v-list-item-content>
+                </v-list-item>
+
+                <v-list-item>
+                  <v-list-item-content>Price :</v-list-item-content>
+                  <v-list-item-content class="align-end">{{ restaurant.price_range }}/5</v-list-item-content>
+                </v-list-item>
+
+                <v-list-item>
+                  <v-list-item-content>Cuisine :</v-list-item-content>
+                  <v-list-item-content class="align-end">{{ restaurant.cuisines }}</v-list-item-content>
+                </v-list-item>
+
+                <v-list-item>
+                  <v-list-item-content>price for 2 :</v-list-item-content>
+                  <v-list-item-content class="align-end">{{ restaurant.average_cost_for_two }}</v-list-item-content>
+                </v-list-item>
+
+                <v-list-item>
+                  <v-list-item-content>Phone :</v-list-item-content>
+                  <v-list-item-content class="align-end">{{ restaurant.phone_numbers }}</v-list-item-content>
+                </v-list-item>
+
+                <v-list-item>
+                  <v-list-item-content>delivering now</v-list-item-content>
+                  <v-list-item-content class="align-end">{{ restaurant.is_delivering_now }}</v-list-item-content>
+                </v-list-item>
+
+                <v-list-item>
+                  <v-list-item-content>highlight :</v-list-item-content>
+                  <v-list-item-content class="align-end">{{ restaurant.highlights[0] }}</v-list-item-content>
+                </v-list-item>
+              </v-list>
+            </v-card>
+</template>
+
+<script>
+  
+  export default {
+    
+    name: 'RestauItem',
+    props : {restaurant : Object},
+    
+    data: () => ({
+      
+    }),
+    mounted(){
+      this.restaurant.phone_numbers = this.restaurant.phone_numbers.split(',')[0]
+      this.restaurant.cuisines = this.restaurant.cuisines.split(',')[0]
+
+      console.log(this.restaurant)
+    }
+  }
+  </script>
+  <style>
+  .floatR{
+    float:right;
+    width: 100%;
+    height: 250px;;
+    margin-bottom: 20px;
+  }
+  .title-container{
+    position:relative
+  }
+  </style>
